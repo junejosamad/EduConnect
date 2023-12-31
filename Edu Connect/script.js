4f7ab0ba8c604818bd6a634124b0a04f8813c382
Abdul Samad <86289220+junejosamad@users.noreply.github.com>
2024-01-01 03:20:04 +0500

    Update script.js

---
+++ Edu Connect/script.js
@@ -0,0 +1,12 @@
+function login(){
+    alert( "Sorry\nThere is a server issue\nYou can't login.");
+}
+
+function search(){
+    
+    alert("University not found");
+}
+
+function submit(){
+    alert("Submited");
+}
